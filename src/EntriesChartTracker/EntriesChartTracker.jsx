010aa2824559fc016bf04bbcb81c8707034af371
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-07-22 14:31:21 -0500

    Clear up some files

--- src/EntriesChartTracker/EntriesChartTracker.jsx
+++ src/EntriesChartTracker/EntriesChartTracker.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
 import AddEntryForm from "./Components/AddEntry/AddEntryFrom";
+import EntriesChartTracker from "./EntriesChartTracker/EntriesChartTracker";
 
 function App() {
   const [entries, setEntries] = useState([
@@ -17,7 +18,8 @@ function App() {
   return (
     <div>
       <DisplayEntries parentEntries={entries} />
-      <AddEntryForm addNewEntryProperty={addNewEntry} />
+      <AddEntryForm addNewEntry={addNewEntry} />
+      <EntriesChartTracker parentEntries={entries} />
     </div>
   );
 }
