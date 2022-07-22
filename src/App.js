475c33f0cc9d6d77f243c74dfd0288ce68a685fd
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-07-22 15:27:43 -0500

    Complete AddEntryForm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
 import AddEntryForm from "./Components/AddEntry/AddEntryFrom";
+import EntriesChartTracker from "./EntriesChartTracker/EntriesChartTracker";
 
 function App() {
   const [entries, setEntries] = useState([
