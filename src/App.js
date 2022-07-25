a4d881a2b88271649f06d29bd34c8856f539ac49
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-07-25 11:27:51 -0500

    Created Forms and Color Tables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
-import AddEntryForm from "./Components/AddEntry/AddEntryFrom";
+import AddEntryForm from "./Components/AddEntry/AddEntryForm";
 import EntriesChartTracker from "./EntriesChartTracker/EntriesChartTracker";
+import "./App.css";
 
 function App() {
   const [entries, setEntries] = useState([
@@ -18,12 +19,22 @@ function App() {
   return (
     <div className="container-fluid">
       <div className="row">
+        <h3 style={{ margin: "1em" }}>
+          Weight
+          <small className="text-muted">Tracker</small>
+        </h3>
         <div className="col-md-6">
-          <DisplayEntries parentEntries={entries} />
-          <AddEntryForm addNewEntryProperty={addNewEntry} />
+          <div className="border-box">
+            <DisplayEntries parentEntries={entries} />
+          </div>
+          <div className="border-box">
+            <AddEntryForm addNewEntryProperty={addNewEntry} />
+          </div>
         </div>
         <div className="col-md-6">
-          <EntriesChartTracker parentEntries={entries} />
+          <div className="border-box">
+            <EntriesChartTracker parentEntries={entries} />
+          </div>
         </div>
       </div>
     </div>
